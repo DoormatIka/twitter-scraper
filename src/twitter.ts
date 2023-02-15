fc71b659949b053f9d5e01ac07b57026d8564c4d
DoormatIka <trashcanartistlily@gmail.com>
2023-02-15 22:52:34 +0800

    removed example from twitter.ts

--- src/twitter.ts
+++ src/twitter.ts
@@ -33,24 +33,3 @@ class TwitterUser {
         return `TwitterUser ${this.at}`
     }
 }
-async function main() {
-    const browser = new CustomBrowser();
-    await browser.init({
-        headless: true, 
-        execPath: "C:/Program Files/Google/Chrome/Application/chrome.exe" 
-    })
-    const lilyn = new TwitterUser(browser, "LilynHana");
-    await lilyn.init();
-
-    // get profile info!
-    await lilyn.getProfile();
-
-    // get tweets every n scroll down
-    await lilyn.getTweetsbyPage(2);
-
-    // https://twitter.com/LilynHana/status/1624553417120022531
-    // gets every tweets until you encounter that tweet.
-    await lilyn.getTweetsUntilID("1624553417120022531");
-
-    await browser.close()
-}
