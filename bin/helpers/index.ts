8509dff14cfe09e0b1f375d9415c415a0f2e6806
DoormatIka <trashcanartistlily@gmail.com>
2023-02-20 14:14:02 +0800

    facade for helpers folder

---
+++ bin/helpers/index.ts
@@ -0,0 +1,9 @@
+import { browsered } from "./browser";
+import { wait, round, storePrint } from "./misc";
+import { tabMaker } from "./tab";
+
+export {
+    browsered,
+    wait, round, storePrint,
+    tabMaker,
+}
